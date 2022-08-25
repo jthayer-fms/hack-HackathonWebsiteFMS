bd99ed4acd1fd68e50d6af55f7587ce6bc9b7b1d
fpop <fpop@fmsystems.com>
2022-08-25 13:54:42 +0300

    added data persistency, tested methods

--- backend/HackathonService/HackathonService/Domains/Pitch.cs
+++ backend/HackathonService/HackathonService/Domains/Pitch.cs
@@ -1,53 +1,68 @@
-﻿namespace HackathonService.Dtos
+﻿using HackathonService.RequestDtos;
+
+namespace HackathonService.Dtos
 {
-    public class  Pitch
+    public class Pitch
     {
-        public Pitch(Guid eventId, Guid id, string description, User creator)
+        public Pitch(Guid eventId, Guid id, string description)
         {
             this.eventId = eventId;
             this.id = id;
             this.description = description;
-            this.creator = creator;
+
+        }
+        public Pitch(Guid eventId, CreatePitchRequest request)
+        {
+            this.eventId = eventId;
+            this.creationTime = request.creationTime;
+            this.creator = request.Creator;
+            this.description = request.description;
+            this.id = Guid.NewGuid();
+
         }
 
-        public void signUp(string user)
+        public void signUp(User user)
         {
-            if (!teamMembers.Contains(user))
+            if (!pitchMembers.Any(pm => pm.userId == user.id))
             {
-                teamMembers?.Add(user);
+                pitchMembers.Add(new PitchMember { userId = user.id, pitchId = id });
             }
             else
-            { 
-                throw new Exception("the user signed up"); 
+            {
+                throw new Exception("the user signed up");
             }
 
-            if (teamMembers.Count >= 4)
+            if (pitchMembers.Count >= 4)
             {
                 throw new Exception("the pitch has maximum of 4 members");
             }
 
         }
 
-        public  void Vote()
+        public void Vote()
         {
             votes += 1;
         }
 
-        public  void UnVote()
+        public void UnVote()
         {
             votes -= 1;
         }
 
         public Guid eventId { get; set; }
+
         public Guid id { get; set; }
+
         public string? description { get; set; }
 
         public DateTime creationTime { get; } = DateTime.UtcNow;
 
-        public User? creator { get; set; }
+        public int creatorId { get; set; }
 
-        public List<string>? teamMembers { get; set; } = new List<string>();
+        public User creator { get; set; }
 
-        public  int votes { get; set; } = 0;
+        public IList<PitchMember> pitchMembers { get; set; }
+
+        public int votes { get; set; } = 0;
     }
 }
--- backend/HackathonService/HackathonService/Providers/PitchProvider.cs
+++ backend/HackathonService/HackathonService/Providers/PitchProvider.cs
@@ -1,32 +1,48 @@
 ﻿using HackathonService.Dtos;
+using HackathonService.Persistency;
 
 namespace HackathonService.Providers
 {
-    public class PitchProvider: IPitchProvider
+    public class PitchProvider : IPitchProvider
     {
-        public PitchProvider()
-        { 
+        public HackContext _context;
+
+        public PitchProvider(HackContext context)
+        {
+            _context = context;
         }
 
         public async Task<Pitch> SignUp(Guid id, User user)
         {
             //mock the pitch dto
             var pitch = await GetById(id);
-            pitch.signUp(user.name);
+            
+            pitch.signUp(user);
+            
+            _context.SaveChanges();
+        
             return pitch;
         }
 
         public async Task<Pitch> GetById(Guid id)
         {
-            var obj = new Pitch(Guid.NewGuid(), Guid.NewGuid(), "Hackathon Website", new User("Michael Crawford"));
-            return obj;
+            //  var obj =
+            var pitch = await _context.Pitches.FindAsync(id);
+            if (pitch != null)
+            {
+                return pitch;
+            }
+            else {
+                return new Pitch(Guid.NewGuid(), Guid.NewGuid(), "Hackathon Website");
+            }
         }
 
         // negative test
         public async Task<Pitch> GetByIdReturnInvalidOne(Guid id)
         {
-            var obj = new Pitch(Guid.NewGuid(), Guid.NewGuid(), "Hackathon Website", new User("Michael Crawford"));
-            obj.teamMembers.Add("Michael Crawford");
+            var obj = new Pitch(Guid.NewGuid(), Guid.NewGuid(), "Hackathon Website");
+            var user = new User("Michael Crawford");
+            obj.pitchMembers.Add(new PitchMember { pitchId = obj.id, userId = user.id });
             return obj;
         }
 
